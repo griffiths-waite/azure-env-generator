18e3295d0abd3043f58899cc15c8d7b2e31cfcdb
Joseph Tuffin <joseph.tuffin@gmail.com>
2025-08-13 11:09:54 +0100

    Added boilerplate

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    dts: true,
+    clean: true,
+    sourcemap: true,
+    outDir: "dist",
+    format: ["esm", "cjs"],
+    entry: ["./src/index.ts"],
+});
