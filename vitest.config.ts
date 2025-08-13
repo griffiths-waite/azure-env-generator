62cd677577ec232b59c7d1e6758defe4e3b0d9fd
Joseph Tuffin <joseph.tuffin@gmail.com>
2025-08-13 12:12:27 +0100

    Added vitest boilerplate

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        setupFiles: ["tests/setup.ts"],
+        include: ["**/*.test.ts"],
+        root: "src",
+        reporters: ["default"],
+        coverage: {
+            provider: "istanbul",
+            reporter: ["lcov", "html", "text"],
+            reportsDirectory: "../coverage",
+            exclude: ["**/dev.ts"],
+        },
+    },
+});
